498cb0a24179bd697afaf2706736c35f79c1ca4e
Kevia <jhampac@inbox.com>
2016-01-18 14:33:50 -0800

    added social compose view controller

--- StormViewer/StormViewer/DetailViewController.swift
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -6,17 +6,18 @@
 //  Copyright © 2016 jhampac. All rights reserved.
 //
 
+import Social
 import UIKit
 
 class DetailViewController: UIViewController
 {
-    
     @IBOutlet weak var detailImageView: UIImageView!
 
     var detailItem: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "shareTapped")
         configureView()
     }
     
@@ -37,6 +38,15 @@ class DetailViewController: UIViewController
         {
             self.detailImageView.image = UIImage(named: imageName)
         }
+    }
+    
+    func shareTapped()
+    {
+        let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        vc.setInitialText("Look at this great picture!")
+        vc.addImage(detailImageView.image!)
+        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+        presentViewController(vc, animated: true, completion: nil)
     }
 }
 
